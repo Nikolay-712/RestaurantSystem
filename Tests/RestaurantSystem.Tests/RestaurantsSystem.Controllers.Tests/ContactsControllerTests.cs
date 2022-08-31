893a5b427742b3f24e159891c26c0a9832000f53
Nikolay <nikolay--georgiev@abv.bg>
2022-08-31 19:59:39 +0300

    Edit controller tests

--- Tests/RestaurantSystem.Tests/RestaurantsSystem.Controllers.Tests/ContactsControllerTests.cs
+++ Tests/RestaurantSystem.Tests/RestaurantsSystem.Controllers.Tests/ContactsControllerTests.cs
@@ -1,6 +1,5 @@
 ﻿using RestaurantSystem.Data;
 using RestaurantSystem.Services.Contacts;
-using RestaurantSystem.Services.Notifications;
 using RestaurantSystem.Services.Reservations;
 using RestaurantSystem.Web.Controllers.Contacts;
 
@@ -9,37 +8,84 @@ using Moq;
 using Xunit;
 using RestaurantSystem.Web.ViewModels.Contacts;
 using RestaurantSystem.Data.Models.Contacts;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using System.Net;
-using Microsoft.AspNetCore.Mvc.Abstractions;
+using RestaurantSystem.Data.Models.Users;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc;
 
 namespace RestaurantSystem.Tests.RestaurantsSystem.Controllers.Tests
 {
     public class ContactsControllerTests
     {
         private ApplicationDbContext dbContext;
-        private ContactService contactService;
+        private ContactsController contactController;
 
         public ContactsControllerTests()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                 .UseInMemoryDatabase(databaseName: "ApplicationTestDb").Options;
-
             this.dbContext = new ApplicationDbContext(options);
-            var notificationService = new Mock<INotificationService>();
-            this.contactService = new ContactService(this.dbContext, notificationService.Object);
+
+            var reservationService = new Mock<IReservationService>();
+            var contactService = new Mock<IContactService>();
+
+            this.contactController = new ContactsController(contactService.Object, reservationService.Object);
         }
 
         [Fact]
-        public void ControllerTest()
+        public async Task SendMessageWithInvalidModelState()
         {
-            var reservationService = new Mock<IReservationService>();
-            var controller = new ContactsController(this.contactService, reservationService.Object);
-           
-            controller.Index();
+            var testUser = await this.AddTestUserAsync();
+            this.AddTestClaims(testUser.Email, testUser.Id);
 
+            var mockTempData = new Mock<ITempDataDictionary>();
+            this.contactController.TempData = mockTempData.Object;
+
+            this.contactController.ModelState.AddModelError("emptyMessage", "some text");
+
+            var result = await this.contactController
+                .Index(new MessageInputVewModel 
+                {
+                    Message = "eeeeeeeeeeeee",
+                    MessageType = MessageType.Саобщение,
+                });
+
+           var aaa= contactController.ViewData.ModelState.Count;
+
+            Assert.True(!contactController.ModelState.IsValid, "This must be InValid");
+        }
+
+
+        private void AddTestClaims(string email, string userId)
+        {
+            var claims = new List<Claim>()
+             {
+                 new Claim(ClaimTypes.Name, email),
+                 new Claim(ClaimTypes.NameIdentifier, userId),
+             };
+
+            var identity = new ClaimsIdentity(claims, "TestAuthType");
+            var user = new ClaimsPrincipal(identity);
+
+            this.contactController
+                .ControllerContext
+                .HttpContext = new DefaultHttpContext { User = user };
+        }
+
+        private async Task<ApplicationUser> AddTestUserAsync()
+        {
+            await this.dbContext.Users.AddAsync(new ApplicationUser
+            {
+                CreatedOn = DateTime.UtcNow,
+                Email = "testUser@abv.bg",
+                IsDeleted = false,
+                PasswordHash = "123456"
+            });
+
+            await this.dbContext.SaveChangesAsync();
+            return this.dbContext.Users.FirstOrDefault();
         }
     }
 }
+
