25222a5293087436c97babbeeb57eef6c9f90cec
Nikolay <nikolay--georgiev@abv.bg>
2022-09-07 15:23:42 +0300

    Integrate online payments with Stripe

--- Services/RestaurantSystem.Services/Payments/PaymentService.cs
+++ Services/RestaurantSystem.Services/Payments/PaymentService.cs
@@ -1,19 +1,25 @@
 ﻿namespace RestaurantSystem.Services.Payments
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
+    using Microsoft.Extensions.Configuration;
     using RestaurantSystem.Data;
     using RestaurantSystem.Data.Models.Payments;
     using RestaurantSystem.Web.ViewModels.Payments;
+    using Stripe;
 
     public class PaymentService : IPaymentService
     {
         private readonly ApplicationDbContext applicationDbContext;
+        private readonly IConfiguration configuration;
 
-        public PaymentService(ApplicationDbContext applicationDbContext)
+        public PaymentService(ApplicationDbContext applicationDbContext, IConfiguration configuration)
         {
             this.applicationDbContext = applicationDbContext;
+            this.configuration = configuration;
         }
 
         public async Task<string> MakePaymentAsync(string orderId, PaymentsInputModel paymentsInput)
@@ -31,6 +37,92 @@
                     payment.IsSuccessful = true;
                     break;
                 case PaymentType.DebitCard:
+
+                    var processPaymentResult = new ProcessPaymentResult();
+                    try
+                    {
+                        var customers = new CustomerService();
+
+
+                        var options = new RequestOptions
+                        {
+                            ApiKey = "",
+                        };
+
+                        var optionToken = new TokenCreateOptions
+                        {
+                            Card = new TokenCardOptions
+                            {
+                                Number = paymentsInput.CardNumber,
+                                ExpMonth = paymentsInput.Expiration.Value.Month.ToString(),
+                                ExpYear = paymentsInput.Expiration.Value.Day.ToString(),
+                                Cvc = paymentsInput.CVV,
+                                Name = paymentsInput.CardName,
+                                Currency = "bgn",
+                            },
+                        };
+
+                        var tokenService = new TokenService();
+                        Token paymentToken = await tokenService.CreateAsync(optionToken, options);
+
+                        var customer = new Customer();
+                        var customerEmail = "testcustumer@abv.bg";
+
+                        var stripeCustomer = await customers.ListAsync(
+                            new CustomerListOptions
+                            {
+                                Email = customerEmail,
+                                Limit = 1,
+                            },
+                            options);
+
+                        if (stripeCustomer.Data.Count == 0)
+                        {
+                            // create new customer
+                            customer = await customers.CreateAsync(
+                                new CustomerCreateOptions
+                                {
+                                    Source = paymentToken.Id,
+                                    Phone = "",
+                                    Name = "custumerTestName".ToString(),
+                                    Email = customerEmail,
+                                    Description = $"test description",
+                                },
+                                options);
+                        }
+                        else
+                        {
+                            // use existing customer
+                            customer = stripeCustomer.FirstOrDefault();
+                        }
+
+                        var charges = new ChargeService();
+                        var charge = await charges.CreateAsync(
+                            new ChargeCreateOptions
+                            {
+                                Source = paymentToken.Id,
+                                Amount = 1000,
+                                Currency = "bgn",
+                                ReceiptEmail = "test@abv.bg",
+                                Description = "new test description",
+                            },
+                            options);
+
+                        if (charge.Status.ToLower().Equals("succeeded"))
+                        {
+                            payment.IsSuccessful = true;
+                        }
+                        else
+                        {
+                            processPaymentResult.Errors.Add("Error processing payment." + charge.FailureMessage);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        processPaymentResult.Errors.Add(ex.Message);
+                    }
+
                     break;
                 default:
                     break;
@@ -46,5 +138,12 @@
 
             return payment.Id;
         }
+
     }
+
+    public class ProcessPaymentResult
+    {
+        public List<string> Errors { get; set; }
+    }
+
 }
